2116356b21ee7b898f8f9121231fde5fcadff2cd
Devoted Llama <devotedllamadev@gmail.com>
2025-03-08 23:06:59 +0000

    Add projects and organise folders
    Add the following projects:
    - Room Coordinate
    - War Preparation
    - ColouredItems

---
+++ Part 2 Object Oriented Programming/ColouredItems/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+ColouredItem<Sword> blueSword = new(ConsoleColor.Blue);
+ColouredItem<Bow> redBow = new(ConsoleColor.Red);
+ColouredItem<Axe> greenAxe = new(ConsoleColor.Green);
+
+blueSword.Display();
+redBow.Display();
+greenAxe.Display();
+
+public class Sword {}
+public class Bow {}
+public class Axe {}
+
+public class ColouredItem<T> where T : new() {
+    public T Item { get ; }
+    public ConsoleColor Colour { get ; }
+    public ColouredItem(ConsoleColor colour) {
+        Item = new T();
+        Colour = colour;
+    }
+
+    public void Display() {
+        Console.ForegroundColor = Colour;
+        Console.WriteLine($"{Item}");
+    }
+}
---
+++ Part 2 Object Oriented Programming/WarPreparations/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Sword basicSword = new Sword(Material.iron, Gemstone.none, 80, 25);
+Sword goodSword = basicSword with {material = Material.steel};
+Sword bestSword = goodSword with {gemstone = Gemstone.bitstone} ;
+Console.WriteLine(basicSword);
+Console.WriteLine(goodSword);
+Console.WriteLine(bestSword);
+
+public enum Material { wood, bronze, iron, steel, binarium}
+public enum Gemstone {none, emerald, amber, sapphire, diamond, bitstone}
+
+public record Sword (Material material, Gemstone gemstone, float length, float crossGuardWidth) {}
+
+
