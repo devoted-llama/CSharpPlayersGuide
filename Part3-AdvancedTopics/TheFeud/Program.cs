89d6f9ec1cef7f4661f4df3bb17a8d6d92a9f781
Devoted Llama <devotedllamadev@gmail.com>
2025-03-12 21:52:54 +0000

    Do "The Feud" challenge.

---
+++ Part3-AdvancedTopics/TheFeud/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Flame;
+using Frost;
+using FrostPig = Frost.Pig;
+
+new Sheep();
+new Flame.Pig();
+new Cow();
+new FrostPig();
+
+namespace Flame {
+    public class Sheep {}
+    public class Pig {}
+}
+
+namespace Frost {
+    public class Cow {}
+    public class Pig {}
+}
+
