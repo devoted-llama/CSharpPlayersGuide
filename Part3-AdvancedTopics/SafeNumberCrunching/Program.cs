5cdaa2e5ae395bf2338317ec1a2bf8ec042af00b
Devoted Llama <devotedllamadev@gmail.com>
2025-03-14 18:16:09 +0000

    Do the "Safer Number Crunching" Challenge.

---
+++ Part3-AdvancedTopics/SafeNumberCrunching/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int value;
+bool isParsed;
+string? input;
+Console.WriteLine("Hello. Please enter a whole number.");
+do {
+    input = Console.ReadLine();
+    isParsed = int.TryParse(input, out value);
+
+    if(isParsed == false) {
+        Console.WriteLine("Incorrect. Try again.");
+    }else {
+        Console.WriteLine($"Thank you for entering the value {value}.");
+    }
+} while(isParsed == false);
+
+double value2;
+Console.WriteLine("Hello. Please enter a floating point number.");
+do {
+    input = Console.ReadLine();
+    isParsed = double.TryParse(input, out value2);
+
+    if(isParsed == false) {
+        Console.WriteLine("Incorrect. Try again.");
+    }else {
+        Console.WriteLine($"Thank you for entering the value {value2}.");
+    }
+} while(isParsed == false);
+
+bool value3;
+Console.WriteLine("Hello. Please enter a boolean.");
+do {
+    input = Console.ReadLine();
+    isParsed = bool.TryParse(input, out value3);
+
+    if(isParsed == false) {
+        Console.WriteLine("Incorrect. Try again.");
+    }else {
+        Console.WriteLine($"Thank you for entering the value {value3}.");
+    }
+} while(isParsed == false);
