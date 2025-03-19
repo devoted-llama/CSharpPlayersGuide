6084fb64f8c5e0d561d934647ff100e178bb4162
Devoted Llama <devotedllamadev@gmail.com>
2025-03-19 16:48:48 +0000

    Do "Indexing Operand City" challenge

--- Part3-AdvancedTopics/NavigatingOperandCity/Program.cs
+++ Part3-AdvancedTopics/NavigatingOperandCity/Program.cs
@@ -1,15 +1,25 @@
 ﻿// Code to test all operators
 
+using System.ComponentModel;
+using System.Net.Http.Headers;
+
 BlockCoordinate startingCoords = new BlockCoordinate(4,3);
 BlockOffset offset = new BlockOffset(2,0);
 BlockCoordinate newCoords = startingCoords + offset;
 if(newCoords.Row == 6 && newCoords.Column == 3) Console.WriteLine($"Pass. Got {newCoords}."); 
 else Console.WriteLine($"Fail. Got {newCoords}.");
 
-
 newCoords = startingCoords + Direction.East;
 if(newCoords.Row == 4 && newCoords.Column == 4) Console.WriteLine($"Pass. Got {newCoords}."); 
 else Console.WriteLine($"Fail. Got {newCoords}.");
+
+Console.WriteLine($"{newCoords[0]}{newCoords[1]}");
+
+try {
+    Console.WriteLine(newCoords[2]);
+} catch (Exception e){
+    Console.WriteLine(e);
+}
 
 public enum Direction {North,East,South,West};
 public record BlockOffset (int RowOffset, int ColumnOffset);
@@ -29,4 +39,14 @@ public record BlockCoordinate(int Row, int Column) {
     }
     public static BlockCoordinate operator +(Direction d, BlockCoordinate c) =>
         c + d;
+
+    public double this[int number] {
+        get {
+            return number switch {
+                0 => this.Row,
+                1 => this.Column,
+                _ => throw new IndexOutOfRangeException() 
+            };
+        }
+    }
 }
